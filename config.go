762989bc165025545c8e6e4def44096ed550e80d
loncat <me@lcat.dev>
2020-03-25 10:46:32 +0800

    Added humanRedableValues method

--- config.go
+++ config.go
@@ -11,16 +11,8 @@ type config struct {
 	ClientName         string            `yaml:"client_name"`
 	DbConnectionString string            `yaml:"db_connection_string"`
 	SessionFilename    string            `yaml:"session_filename"`
-	Days               map[string]int    `yaml:"days"`
+	Days               map[int]string    `yaml:"days"`
 	MessageTemplates   map[string]string `yaml:"message_templates"`
-}
-
-func (c config) getMessageTemplate(key string) string {
-	template, ok := c.MessageTemplates[key]
-	if !ok {
-		return ""
-	}
-	return template
 }
 
 func loadConfig(c *config) {
@@ -31,4 +23,29 @@ func loadConfig(c *config) {
 	decoder := yaml.NewDecoder(f)
 	err = decoder.Decode(&c)
 	checkError(err)
+}
+
+func (c config) getMessageTemplate(key string) string {
+	template, ok := c.MessageTemplates[key]
+	if !ok {
+		return ""
+	}
+	return template
+}
+
+func (c config) getDayByName(name string) (int, bool) {
+	for k, v := range c.Days {
+		if v == name {
+			return k, true
+		}
+	}
+	return 0, false
+}
+
+func (c config) getNameByDay(num int) (string, bool) {
+	name, ok := c.Days[num]
+	if !ok {
+		return "", false
+	}
+	return name, true
 }
